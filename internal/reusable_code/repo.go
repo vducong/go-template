d2019dffbc4d57000666204bff163ef7612c93bc
VCD <vdc1114@gmail.com>
2023-06-07 23:37:24 +0700

    refactor: write repo with model for reusability

--- internal/reusable_code/repo.go
+++ internal/reusable_code/repo.go
@@ -16,13 +16,17 @@ func newRepo(db databases.MySQLDB) *Repo {
 	return &Repo{db}
 }
 
-func (r *Repo) dbWithContext(ctx context.Context) *gorm.DB {
+func (r *Repo) dbCtxWithModel(ctx context.Context) *gorm.DB {
+	return r.db.WithContext(ctx).Model(&ReusableCode{})
+}
+
+func (r *Repo) dbCtxRaw(ctx context.Context) *gorm.DB {
 	return r.db.WithContext(ctx)
 }
 
 func (r *Repo) GetByCode(ctx context.Context, code string) (*ReusableCode, error) {
 	var rc ReusableCode
-	if err := r.dbWithContext(ctx).Where(&ReusableCode{Code: code}).Take(&rc).
+	if err := r.dbCtxWithModel(ctx).Where(&ReusableCode{Code: code}).Take(&rc).
 		Error; err != nil {
 		return nil, failure.ErrWithTrace(err)
 	}
